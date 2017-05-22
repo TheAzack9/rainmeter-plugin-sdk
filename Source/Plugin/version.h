70dd4a60f3c89f601b84b00a4970048f70221db8
theAzack9 <robin@cavachi.be>
2017-05-22 14:59:19 +0200

    tweaks

--- Source/Plugin/version.h
+++ Source/Plugin/version.h
@@ -1,8 +1,8 @@
 #define PLUGIN_VERSION "1.0.0.0"
-#define PLUGIN_AUTHOR "theAzack9"
+#define PLUGIN_AUTHOR "YOURNAMEHERE"
 #define PLUGIN_YEAR 2017
 // Has to be the same as the project name!
-#define PLUGIN_NAME "Animator"
+#define PLUGIN_NAME "Plugin"
 // This is the path relative to root directory (where build.bat is placed)
 #define EXAMPLE_SKIN_PATH "ExampleSkin"
 #define EXAMPLE_SKIN_NAME "Example Skin"
