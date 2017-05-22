b03cc8fabd6ab0b533a55d5d05f60bba25baa0b1
theAzack9 <robin@cavachi.be>
2017-05-22 14:54:22 +0200

    Updated README, Added more variables to version.h, added redist build system

--- Build.bat
+++ Build.bat
@@ -1,35 +1,77 @@
 @echo off
 setlocal enabledelayedexpansion enableextensions
 
+title Plugin Build Script
+
 rem TODO: fetch more variables from variables.h
-rem AuthorName, Example skin name, Plugin name (which has to be the same as plugin project name!), 
+rem Example skin name, Plugin name (which has to be the same as plugin project name!), 
 
 echo.
 echo.
 echo "Building Plugin"
 echo --------------------
 
-set Plugin=Plugin
-set Project=Source\%Plugin%
+set Project=Source\Plugin
 
 echo Finding version for plugin
 for /f "delims=" %%x in (%Project%\version.h) do ( 
-	echo %%x|findstr "PLUGIN_VERSION " >nul 2>&1
+	echo %%x|findstr "PLUGIN_VERSION" >nul 2>&1
 	if not errorlevel 1 (
 		set Version=%%x
 	)
-	rem if not x%str1:PLUGIN_VERSION=%==x%str1% echo It contains PLUGIN_VERSION
+
+	echo %%x|findstr "PLUGIN_AUTHOR" >nul 2>&1
+	if not errorlevel 1 (
+		set Author=%%x
+	)
+
+	echo %%x|findstr "PLUGIN_NAME" >nul 2>&1
+	if not errorlevel 1 (
+		set Plugin=%%x
+	)
+
+	echo %%x|findstr "EXAMPLE_SKIN_PATH" >nul 2>&1
+	if not errorlevel 1 (
+		set SkinPath=%%x
+	)
+	
+	echo %%x|findstr "EXAMPLE_SKIN_NAME" >nul 2>&1
+	if not errorlevel 1 (
+		set SkinName=%%x
+	)
 )
 
 if not DEFINED Version (
-		echo Could not find Version
+		echo Could not determine Version
 		echo Try running build with version number as first argument to override
 		goto END
 	)  
 
+if not DEFINED Author (
+		echo Could not determine Author
+		echo Try running build with author as second argument to override
+		goto END
+	)
+
 set Version=%Version:#define PLUGIN_VERSION =%
 set "Version=%Version:*"=%
 echo Version found: %Version%
+
+set Author=%Author:#define PLUGIN_AUTHOR =%
+set "Author=%Author:*"=%
+echo Author found: %Author%
+
+set Plugin=%Plugin:#define PLUGIN_NAME =%
+set "Plugin=%Plugin:*"=%
+echo Plugin found: %Plugin%
+
+set SkinPath=%SkinPath:#define EXAMPLE_SKIN_PATH =%
+set "SkinPath=%SkinPath:*"=%
+echo SkinPath found: %SkinPath%
+
+set SkinName=%SkinName:#define EXAMPLE_SKIN_NAME =%
+set "SkinName=%SkinName:*"=%
+echo SkinName found: %SkinName%
 
 echo.
 
@@ -67,8 +109,23 @@ echo Removing build log
 del "BuildLog.txt"
 
 echo Packing rmskin:
-call "CreateSkinPluginPackage.exe" %Plugin% theAzack9 "%Version%" "Example Skin" "%ReleaseLocation%\%Plugin%.rmskin" "ExampleSkin\\" "%ReleaseLocation%\x86\%Plugin%.dll" "%ReleaseLocation%\x64\%Plugin%.dll"
+call "PluginPacker.exe" "Create Skin" "%Plugin%" "%Author%" "%Version%" "%SkinName%" "%ReleaseLocation%\%Plugin% %SkinName%.rmskin" "%SkinPath%/" "%ReleaseLocation%\x86\%Plugin%.dll" "%ReleaseLocation%\x64\%Plugin%.dll"
 
-echo "Finished sucessfully"
+if errorlevel 1 (
+	echo Could not pack skin, Aborting build
+	rmdir /S /Q "%~dp0%ReleaseLocation%/"
+	goto END
+	)
+
+echo Packing redist:
+call "PluginPacker.exe" "Create Redist" "%Plugin%" "%ReleaseLocation%\%Plugin% Redist.zip" "%ReleaseLocation%\x86\%Plugin%.dll" "%ReleaseLocation%\x64\%Plugin%.dll"
+
+if errorlevel 1 (
+	echo Could not pack redist, Aborting build
+	rmdir /S /Q "%~dp0%ReleaseLocation%/"
+	goto END
+	)
+
+echo Finished sucessfully
 :END
 pause
